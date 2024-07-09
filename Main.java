ff555a14b2ba50f30e9441f6f3757af9d5122cd2
ahmed <ahmedmansour10122000@gmail.com>
2024-07-09 15:29:38 +0300

    edit line hello

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class PrintITI {
         public static void main(String[] args) {
             System.out.println("Hello Ahmed");
-            System.out.println("Hello branch ahmed ");
+            System.out.println("branch all is");
         }
     }
