f37c3a6578c8596fc2271c97fd5682891db22130
ahmed <ahmedmansour10122000@gmail.com>
2024-07-09 13:50:04 +0300

    add print hello branch

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class PrintITI {
         public static void main(String[] args) {
             System.out.println("Hello Ahmed");
+            System.out.println("Hello branch ahmed ");
         }
     }
